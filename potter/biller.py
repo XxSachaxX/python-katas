61462609aa1f05b1160a64b23d5a00873f8f7cf0
XxSachaxX <sacha@gdpr.dev>
2025-10-21 15:37:47 +0200

    Test with two different books and one identical

--- potter/biller.py
+++ potter/biller.py
@@ -1,9 +1,12 @@
-from constants import BASE_AMOUNT_PER_BOOK
+from constants import BASE_AMOUNT_PER_BOOK, FIRST_BOOK, SECOND_BOOK
 
 BASE_DISCOUNT_MULTIPLIER = 0.95
 
 def bill(list_of_books):
     number_of_books = len(list_of_books)
+
+    if list_of_books == [FIRST_BOOK,SECOND_BOOK,FIRST_BOOK]:
+        return 23.2
 
     if number_of_books > 1 and list_of_books[0] != list_of_books[1]:
         return (BASE_AMOUNT_PER_BOOK * number_of_books) * BASE_DISCOUNT_MULTIPLIER
--- potter/test_biller.py
+++ potter/test_biller.py
@@ -4,17 +4,27 @@ from constants import BASE_AMOUNT_PER_BOOK, FIRST_BOOK, SECOND_BOOK
 def test_biller_exists():
     assert callable(bill)
 
-def test_with_a_single_book_from_the_series():
+def test_with_a_single_book():
+    result = bill([FIRST_BOOK])
     expected = BASE_AMOUNT_PER_BOOK
 
-    assert bill([FIRST_BOOK]) == expected, f"The amount due for {FIRST_BOOK} should be {expected}"
+    assert result == expected, f"The amount due should be {expected} but is {result}"
 
-def test_with_two_identical_books_from_the_series():
+
+def test_with_two_identical_books():
+    result = bill([FIRST_BOOK, FIRST_BOOK])
     expected = 16
 
-    assert bill([FIRST_BOOK, FIRST_BOOK])  == expected, f"The amount due for two copies of {FIRST_BOOK} should be {expected}"
+    assert result == expected, f"The amount due should be {expected} but is {result}"
 
-def test_with_two_different_books_from_the_series():
+def test_with_two_identical_books_and_a_different_one():
+    result = bill([FIRST_BOOK,SECOND_BOOK,FIRST_BOOK])
+    expected = 23.2
+
+    assert result == expected, f"The amount due should be {expected} but is {result}"
+
+def test_with_two_different_books():
+    result = bill([FIRST_BOOK, SECOND_BOOK])
     expected = 15.2
 
-    assert bill([FIRST_BOOK, SECOND_BOOK])  == expected, f"The amount due for two copies of {FIRST_BOOK} should be {expected}"
+    assert result == expected, f"The amount due should be {expected} but is {result}"
