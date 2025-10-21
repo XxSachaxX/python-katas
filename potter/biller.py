b2c723ce8e05e0258d51ab831817e8fbc9bcdc36
XxSachaxX <sacha@gdpr.dev>
2025-10-21 14:58:17 +0200

    Compute amount due for a single book of the series

--- potter/biller.py
+++ potter/biller.py
@@ -1,2 +1,4 @@
+BASE_AMOUNT_PER_BOOK = 8
+
 def bill(list_of_books):
-    return
+    return BASE_AMOUNT_PER_BOOK
--- potter/test_biller.py
+++ potter/test_biller.py
@@ -1,4 +1,11 @@
 from biller import bill
+from biller import BASE_AMOUNT_PER_BOOK
 
 def test_biller_exists():
     assert callable(bill)
+
+def test_with_a_single_book_from_the_series():
+    book = "Harry Potter and the Sorcerer's Stone"
+    expected = BASE_AMOUNT_PER_BOOK
+
+    assert bill([book]) == expected, f"The amount due for {book} should be {expected}"
