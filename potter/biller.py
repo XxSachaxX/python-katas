be05667f35823c2a6f6343532d708ff2249c15af
XxSachaxX <sacha@gdpr.dev>
2025-10-21 16:46:23 +0200

    Test with three identical books

--- potter/biller.py
+++ potter/biller.py
@@ -4,11 +4,23 @@ BASE_DISCOUNT_MULTIPLIER = 0.95
 
 def bill(list_of_books):
     number_of_books = len(list_of_books)
+    number_of_different_books = get_number_of_different_books(list_of_books)
 
-    if list_of_books == [FIRST_BOOK,SECOND_BOOK,FIRST_BOOK]:
-        return 23.2
+    if number_of_different_books != 0:
+        if number_of_books == 3:
+            bill_for_different_books = (number_of_different_books * BASE_AMOUNT_PER_BOOK) * BASE_DISCOUNT_MULTIPLIER
 
-    if number_of_books > 1 and list_of_books[0] != list_of_books[1]:
-        return (BASE_AMOUNT_PER_BOOK * number_of_books) * BASE_DISCOUNT_MULTIPLIER
+            return bill_for_different_books + BASE_AMOUNT_PER_BOOK
+        else:
+            return (BASE_AMOUNT_PER_BOOK * number_of_books) * BASE_DISCOUNT_MULTIPLIER
 
     return BASE_AMOUNT_PER_BOOK * number_of_books
+
+def get_number_of_different_books(list_of_books):
+    nb_of_copies_of_first_book = list_of_books.count(FIRST_BOOK)
+    nb_of_copies_of_second_book = list_of_books.count(SECOND_BOOK)
+
+    if nb_of_copies_of_first_book > 0 and nb_of_copies_of_second_book > 0:
+        return 2
+    else:
+        return 0
--- potter/test_biller.py
+++ potter/test_biller.py
@@ -17,14 +17,26 @@ def test_with_two_identical_books():
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
+def test_with_two_different_books():
+    result = bill([FIRST_BOOK, SECOND_BOOK])
+    expected = 15.2
+
+    assert result == expected, f"The amount due should be {expected} but is {result}"
+
 def test_with_two_identical_books_and_a_different_one():
     result = bill([FIRST_BOOK,SECOND_BOOK,FIRST_BOOK])
     expected = 23.2
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
-def test_with_two_different_books():
-    result = bill([FIRST_BOOK, SECOND_BOOK])
-    expected = 15.2
+def test_with_two_identical_books_and_a_different_one_in_a_different_order():
+    result = bill([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK])
+    expected = 23.2
+
+    assert result == expected, f"The amount due should be {expected} but is {result}"
+
+def test_with_three_identical_books():
+    result = bill([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK])
+    expected = 24
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
