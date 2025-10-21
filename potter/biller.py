5e291da5eb199f64d39575ed0a74a7eee6ad2830
XxSachaxX <sacha@gdpr.dev>
2025-10-21 15:15:36 +0200

    Refactor base discount multipplier and number of books

--- potter/biller.py
+++ potter/biller.py
@@ -1,7 +1,11 @@
 from constants import BASE_AMOUNT_PER_BOOK
 
-def bill(list_of_books):
-    if len(list_of_books) > 1 and list_of_books[0] != list_of_books[1]:
-        return (BASE_AMOUNT_PER_BOOK * len(list_of_books)) * 0.95
+BASE_DISCOUNT_MULTIPLIER = 0.95
 
-    return BASE_AMOUNT_PER_BOOK * len(list_of_books)
+def bill(list_of_books):
+    number_of_books = len(list_of_books)
+
+    if number_of_books > 1 and list_of_books[0] != list_of_books[1]:
+        return (BASE_AMOUNT_PER_BOOK * number_of_books) * BASE_DISCOUNT_MULTIPLIER
+
+    return BASE_AMOUNT_PER_BOOK * number_of_books
