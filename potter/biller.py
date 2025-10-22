fc4d28fd0175d4c1b632289fbe26c2f2eb18a01a
XxSachaxX <sacha@gdpr.dev>
2025-10-22 11:46:16 +0200

    Refactor code into billing class

--- potter/biller.py
+++ potter/biller.py
@@ -6,54 +6,58 @@ from constants import (
     FOUR_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER, THREE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER,
     STANDARD_RATE, FIVE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER)
 
-def bill(list_of_books):
-    books_counts_by_discount_multiplier = get_books_counts_by_discount_multiplier(list_of_books)
+class Biller():
+    def __init__(self, list_of_books):
+        self.list_of_books = list_of_books
 
-    total = 0
-    for key, value in books_counts_by_discount_multiplier.items():
-        total += (BASE_AMOUNT_PER_BOOK * value) * key
+    def bill(self):
+        books_counts_by_discount_multiplier = self.get_books_counts_by_discount_multiplier()
 
-    return total
+        total = 0
+        for key, value in books_counts_by_discount_multiplier.items():
+            total += (BASE_AMOUNT_PER_BOOK * value) * key
 
-def get_books_counts(list_of_books):
-    return {
-        FIRST_BOOK: list_of_books.count(FIRST_BOOK),
-        SECOND_BOOK: list_of_books.count(SECOND_BOOK),
-        THIRD_BOOK: list_of_books.count(THIRD_BOOK),
-        FOURTH_BOOK: list_of_books.count(FOURTH_BOOK),
-        FIFTH_BOOK: list_of_books.count(FIFTH_BOOK)
-    }
+        return total
 
-def get_books_counts_by_discount_multiplier(list_of_books):
-    books_counts = get_books_counts(list_of_books)
+    def get_books_counts(self):
+        return {
+            FIRST_BOOK: self.list_of_books.count(FIRST_BOOK),
+            SECOND_BOOK: self.list_of_books.count(SECOND_BOOK),
+            THIRD_BOOK: self.list_of_books.count(THIRD_BOOK),
+            FOURTH_BOOK: self.list_of_books.count(FOURTH_BOOK),
+            FIFTH_BOOK: self.list_of_books.count(FIFTH_BOOK)
+        }
 
-    rates = {
-    STANDARD_RATE: 0,
-    TWO_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0,
-    THREE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0,
-    FOUR_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0,
-    FIVE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0
-    }
+    def get_books_counts_by_discount_multiplier(self):
+        books_counts = self.get_books_counts()
 
-    max_number_of_books = max(list(books_counts.values()))
+        rates = {
+        STANDARD_RATE: 0,
+        TWO_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0,
+        THREE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0,
+        FOUR_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0,
+        FIVE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0
+        }
 
-    for i in range(max_number_of_books):
-        number_of_books_for_round = 0
+        max_number_of_books = max(list(books_counts.values()))
 
-        for key, value in books_counts.items():
-            if value > 0:
-                books_counts[key] -= 1
-                number_of_books_for_round += 1
+        for i in range(max_number_of_books):
+            number_of_books_for_round = 0
 
-        if number_of_books_for_round == 5:
-            rates[FIVE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER] += 5
-        elif number_of_books_for_round == 4:
-            rates[FOUR_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER] += 4
-        elif number_of_books_for_round == 3:
-            rates[THREE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER] += 3
-        elif number_of_books_for_round == 2:
-            rates[TWO_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER] += 2
-        elif number_of_books_for_round == 1:
-            rates[STANDARD_RATE] += 1
+            for key, value in books_counts.items():
+                if value > 0:
+                    books_counts[key] -= 1
+                    number_of_books_for_round += 1
 
-    return rates
+            if number_of_books_for_round == 5:
+                rates[FIVE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER] += 5
+            elif number_of_books_for_round == 4:
+                rates[FOUR_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER] += 4
+            elif number_of_books_for_round == 3:
+                rates[THREE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER] += 3
+            elif number_of_books_for_round == 2:
+                rates[TWO_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER] += 2
+            elif number_of_books_for_round == 1:
+                rates[STANDARD_RATE] += 1
+
+        return rates
--- potter/test_biller.py
+++ potter/test_biller.py
@@ -1,4 +1,4 @@
-from biller import bill, get_books_counts, get_books_counts_by_discount_multiplier
+from biller import Biller
 from constants import (
     BASE_AMOUNT_PER_BOOK, FIRST_BOOK,
     SECOND_BOOK, THIRD_BOOK,
@@ -8,72 +8,80 @@ from constants import (
     FOUR_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER,
     STANDARD_RATE, FIVE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER)
 
-def test_biller_exists():
-    assert callable(bill)
-
 def test_with_a_single_book():
-    result = bill([FIRST_BOOK])
+    biller = Biller([FIRST_BOOK])
+    result = biller.bill()
     expected = BASE_AMOUNT_PER_BOOK
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 
 def test_with_two_identical_books():
-    result = bill([FIRST_BOOK, FIRST_BOOK])
+    biller = Biller([FIRST_BOOK, FIRST_BOOK])
+    result = biller.bill()
     expected = 16
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_two_different_books():
-    result = bill([FIRST_BOOK, SECOND_BOOK])
+    biller = Biller([FIRST_BOOK, SECOND_BOOK])
+    result = biller.bill()
     expected = 15.2
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_two_identical_books_and_a_different_one():
-    result = bill([FIRST_BOOK,SECOND_BOOK,FIRST_BOOK])
+    biller = Biller([FIRST_BOOK,SECOND_BOOK,FIRST_BOOK])
+    result = biller.bill()
     expected = 23.2
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_two_identical_books_and_a_different_one_in_a_different_order():
-    result = bill([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK])
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK])
+    result = biller.bill()
     expected = 23.2
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_three_identical_books():
-    result = bill([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK])
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK])
+    result = biller.bill()
     expected = 24
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_three_different_books():
-    result = bill([FIRST_BOOK,SECOND_BOOK,THIRD_BOOK])
+    biller = Biller([FIRST_BOOK,SECOND_BOOK,THIRD_BOOK])
+    result = biller.bill()
     expected = 21.6
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_four_different_books():
-    result = bill([FIRST_BOOK,SECOND_BOOK,THIRD_BOOK, FOURTH_BOOK])
+    biller = Biller([FIRST_BOOK,SECOND_BOOK,THIRD_BOOK, FOURTH_BOOK])
+    result = biller.bill()
     expected = 25.6
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_four_identical_books():
-    result = bill([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK,FIRST_BOOK])
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK,FIRST_BOOK])
+    result = biller.bill()
     expected = 32
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_ultimate():
-    result = bill([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK,SECOND_BOOK,THIRD_BOOK,THIRD_BOOK,FOURTH_BOOK,FIFTH_BOOK])
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK,SECOND_BOOK,THIRD_BOOK,THIRD_BOOK,FOURTH_BOOK,FIFTH_BOOK])
+    result = biller.bill()
     expected = 51.60
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_get_books_counts():
-    result = get_books_counts([FIRST_BOOK, FIRST_BOOK, SECOND_BOOK, THIRD_BOOK, FOURTH_BOOK, FOURTH_BOOK])
+    biller = Biller([FIRST_BOOK, FIRST_BOOK, SECOND_BOOK, THIRD_BOOK, FOURTH_BOOK, FOURTH_BOOK])
+    result = biller.get_books_counts()
     expected = {
         FIRST_BOOK: 2,
         SECOND_BOOK: 1,
@@ -86,6 +94,7 @@ def test_get_books_counts():
 
 def test_books_counts_by_discount_multiplier_with_five_copies_of_one_book():
     list_of_books = [FIRST_BOOK,FIRST_BOOK,FIRST_BOOK,FIRST_BOOK, FIRST_BOOK]
+    biller = Biller(list_of_books)
 
     expected = {
     STANDARD_RATE: 5,
@@ -95,12 +104,13 @@ def test_books_counts_by_discount_multiplier_with_five_copies_of_one_book():
     FIVE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0
     }
 
-    result = get_books_counts_by_discount_multiplier(list_of_books)
+    result = biller.get_books_counts_by_discount_multiplier()
 
     assert result == expected, f"Expected: {expected} but got: {result}"
 
 def test_books_counts_by_discount_multiplier_with_one_copy_of_two_books():
     list_of_books = [FIRST_BOOK,FIFTH_BOOK]
+    biller = Biller(list_of_books)
 
     expected = {
     STANDARD_RATE: 0,
@@ -110,6 +120,6 @@ def test_books_counts_by_discount_multiplier_with_one_copy_of_two_books():
     FIVE_DIFFERENT_BOOKS_DISCOUNT_MULTIPLIER: 0
     }
 
-    result = get_books_counts_by_discount_multiplier(list_of_books)
+    result = biller.get_books_counts_by_discount_multiplier()
 
     assert result == expected, f"Expected: {expected} but got: {result}"
