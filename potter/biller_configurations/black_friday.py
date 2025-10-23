fd9fdda39e48e4cdacbcf0a1e5a6db2efe5e9a40
XxSachaxX <sacha@gdpr.dev>
2025-10-23 12:00:11 +0200

    Test with different biller configuraton

---
+++ potter/biller_configurations/black_friday.py
@@ -0,0 +1,19 @@
+class BlackFridayBillerConfiguration():
+
+    def base_amount_per_book(self):
+        return 5
+
+    def standard_rate(self):
+        return 1
+
+    def two_books_discount_multiplier(self):
+        return 0.9
+
+    def three_books_discount_multiplier(self):
+        return 0.8
+
+    def four_books_discount_multiplier(self):
+        return 0.7
+
+    def five_books_discount_multiplier(self):
+        return 0.5
--- potter/test_biller.py
+++ potter/test_biller.py
@@ -4,76 +4,89 @@ from constants import (
     SECOND_BOOK, THIRD_BOOK,
     FOURTH_BOOK, FIFTH_BOOK)
 from biller_configurations.default import DefaultBillerConfiguration
+from biller_configurations.black_friday import BlackFridayBillerConfiguration
 
-biller_configuration = DefaultBillerConfiguration()
+default_biller_configuration = DefaultBillerConfiguration()
 
 def test_with_a_single_book():
-    biller = Biller([FIRST_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK], default_biller_configuration)
     result = biller.bill()
-    expected = biller_configuration.base_amount_per_book()
+    expected = default_biller_configuration.base_amount_per_book()
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 
 def test_with_two_identical_books():
-    biller = Biller([FIRST_BOOK, FIRST_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK, FIRST_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 16
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_two_different_books():
-    biller = Biller([FIRST_BOOK, SECOND_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK, SECOND_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 15.2
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_two_identical_books_and_a_different_one():
-    biller = Biller([FIRST_BOOK,SECOND_BOOK,FIRST_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK,SECOND_BOOK,FIRST_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 23.2
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_two_identical_books_and_a_different_one_in_a_different_order():
-    biller = Biller([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 23.2
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_three_identical_books():
-    biller = Biller([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 24
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_three_different_books():
-    biller = Biller([FIRST_BOOK,SECOND_BOOK,THIRD_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK,SECOND_BOOK,THIRD_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 21.6
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_four_different_books():
-    biller = Biller([FIRST_BOOK,SECOND_BOOK,THIRD_BOOK, FOURTH_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK,SECOND_BOOK,THIRD_BOOK, FOURTH_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 25.6
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_four_identical_books():
-    biller = Biller([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK,FIRST_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,FIRST_BOOK,FIRST_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 32
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
 
 def test_with_real_complete_basket():
-    biller = Biller([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK,SECOND_BOOK,THIRD_BOOK,THIRD_BOOK,FOURTH_BOOK,FIFTH_BOOK], biller_configuration)
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK,SECOND_BOOK,THIRD_BOOK,THIRD_BOOK,FOURTH_BOOK,FIFTH_BOOK], default_biller_configuration)
     result = biller.bill()
     expected = 51.60
+
+    assert result == expected, f"The amount due should be {expected} but is {result}"
+
+
+# Test with other biller configuration
+
+black_friday_biller_configuration = BlackFridayBillerConfiguration()
+
+def test_with_real_complete_basket_on_black_friday():
+    biller = Biller([FIRST_BOOK,FIRST_BOOK,SECOND_BOOK,SECOND_BOOK,THIRD_BOOK,THIRD_BOOK,FOURTH_BOOK,FIFTH_BOOK], black_friday_biller_configuration)
+    result = biller.bill()
+    expected = 24.50
 
     assert result == expected, f"The amount due should be {expected} but is {result}"
