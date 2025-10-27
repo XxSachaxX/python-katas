cc32889d3dfa4e8bdaa19ba5e92b501cca11dda0
XxSachaxX <sacha@gdpr.dev>
2025-10-27 13:09:16 +0100

    Add tests with two complete series

--- potter/tests/bundles/test_bundle_maker.py
+++ potter/tests/bundles/test_bundle_maker.py
@@ -193,3 +193,56 @@ def test_with_one_book_from_two_different_series():
     bundle_maker = BundleMaker(inventory)
     result = bundle_maker.get_bundles()
     assert sorted(result) == sorted(expected), f"Expected: {expected}, Got: {result}"
+
+def test_with_two_different_complete_series():
+    inventory = {
+        "Harry Potter": {
+            "Harry Potter and the Sorcerer's Stone": 1,
+            "Harry Potter and the Chamber of Secrets": 1,
+            "Harry Potter and the Prisonner of Azkaban": 1,
+            "Harry Potter and the Goblet of fire": 1,
+            "Harry Potter and the Order of the Phoenix": 1,
+            "Harry Potter and the Half-blood Prince": 1,
+            "Harry Potter and the Deathly Hallows": 1
+
+        },
+        "Realm of the elderlings": {
+            "Assassin's Apprentice": 1,
+            "Royal Assassin": 1,
+            "Assassin's Quest": 1,
+            "Fool's Errand": 1,
+            "Golden Fool": 1,
+            "Fool's Fate": 1,
+            "The Rain Wilds Chronicles: Dragon Keeper": 1,
+            "Dragon Haven": 1,
+            "City of Dragons": 1,
+            "Blood of Dragons": 1,
+            "Fitz and the Fool Trilogy: Fool's Assassin": 1,
+            "Fool's Quest": 1,
+            "Assassin's Fate": 1
+        }
+    }
+
+    harry_potter_full_bundle = [
+        "Harry Potter and the Sorcerer's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisonner of Azkaban",
+        "Harry Potter and the Goblet of fire", "Harry Potter and the Order of the Phoenix", "Harry Potter and the Half-blood Prince", "Harry Potter and the Deathly Hallows"
+    ]
+    realm_of_the_underlings_full_bundle = [
+        "Assassin's Apprentice",
+        "Royal Assassin",
+        "Assassin's Quest",
+        "Fool's Errand",
+        "Golden Fool",
+        "Fool's Fate",
+        "The Rain Wilds Chronicles: Dragon Keeper",
+        "Dragon Haven",
+        "City of Dragons",
+        "Blood of Dragons",
+        "Fitz and the Fool Trilogy: Fool's Assassin",
+        "Fool's Quest",
+        "Assassin's Fate"
+    ]
+    expected = [harry_potter_full_bundle,realm_of_the_underlings_full_bundle]
+    bundle_maker = BundleMaker(inventory)
+    result = bundle_maker.get_bundles()
+    assert sorted(result) == sorted(expected), f"Expected: {expected}, Got: {result}"
