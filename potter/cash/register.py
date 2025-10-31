ee186b12c169a1f153809283b96575c259365437
XxSachaxX <sacha@gdpr.dev>
2025-10-31 13:15:48 +0100

    Add possibility to cash in a cart

--- potter/cash/register.py
+++ potter/cash/register.py
@@ -1,5 +1,33 @@
+from potter.inventory.manager import Manager
+from potter.billing.biller import Biller
+# imports required for __get_books_series_configuration to work
+from potter.books_series_configurations.harry_potter import HarryPotterSeries # noqa: F401
+from potter.books_series_configurations.realm_of_the_elderlings import RealmOfTheElderlingsSeries # noqa: F401
+
 class Register():
 
-    def __init__(self, initial_cash_float, inventory):
+    def __init__(self, initial_cash_float, inventory, biller_configuration):
         self.cash_float = initial_cash_float
         self.inventory = inventory
+        self.biller_configuration = biller_configuration
+
+    def cash_in(self, customer_cart):
+        Manager(self.inventory).remove_books(customer_cart)
+
+        total = 0
+        for series_name, books_with_count in customer_cart.items():
+            books_to_bill = []
+
+            for book, count in books_with_count.items():
+                for i in range(count):
+                    books_to_bill.append(book)
+
+            total += Biller(books_to_bill, self.biller_configuration, self.__get_books_series_configuration(series_name)).bill()
+
+        self.cash_float -= total
+
+    def __get_books_series_configuration(self, series_name):
+        config_name = series_name.title().replace(" ", "") + "Series"
+        cls = globals()[config_name]
+
+        return cls()
--- potter/tests/cash/test_register.py
+++ potter/tests/cash/test_register.py
@@ -1,4 +1,7 @@
 from potter.cash.register import Register
+from potter.biller_configurations.default import DefaultBillerConfiguration
+
+default_configuration = DefaultBillerConfiguration()
 
 def test_has_cash_float():
     initial_cash_float = 20000
@@ -7,7 +10,7 @@ def test_has_cash_float():
             "Harry Potter and the Sorcerer's Stone": 1,
         }
     }
-    register = Register(initial_cash_float, initial_inventory)
+    register = Register(initial_cash_float, initial_inventory, default_configuration)
     actual_cash_float = register.cash_float
 
     assert actual_cash_float == 20000, f"Expected register to have cash float {initial_cash_float}, but has {actual_cash_float}"
@@ -19,8 +22,34 @@ def test_has_inventory():
         }
     }
 
-    initial_cash_float = 20000
-    register = Register(initial_cash_float, initial_inventory)
+    initial_cash_float = 1000
+    register = Register(initial_cash_float, initial_inventory, default_configuration)
     actual_inventory = register.inventory
 
     assert actual_inventory == initial_inventory, f"Expected inventory: {initial_inventory}, got: {actual_inventory}"
+
+def test_cash_in_a_cart_with_a_single_book():
+    initial_inventory = {
+        "Harry Potter": {
+            "Harry Potter and the Sorcerer's Stone": 1,
+        }
+    }
+
+    initial_cash_float = 1000
+    register = Register(initial_cash_float, initial_inventory, default_configuration)
+    customer_cart = {
+        "Harry Potter": {
+            "Harry Potter and the Sorcerer's Stone": 1,
+        }
+    }
+
+    expected_inventory = initial_inventory = {
+        "Harry Potter": {
+            "Harry Potter and the Sorcerer's Stone": 0,
+        }
+    }
+    expected_cash_float = 992
+
+    register.cash_in(customer_cart)
+    assert register.inventory == initial_inventory, f"Expected inventory: {expected_inventory}, got: {register.inventory}"
+    assert register.cash_float == expected_cash_float, f"Expected register to have cash float {expected_cash_float}, but has {register.cash_float}"
